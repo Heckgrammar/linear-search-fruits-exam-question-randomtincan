8f328d3873c441566efaf736ba0e691f0ffbbad0
gapple <22dmasood@heckgrammar.co.uk>
2025-05-13 19:30:52 +0100

    Update Program.cs
    half done debug last line find what | is

--- LinearSearchFruitsTask/Program.cs
+++ LinearSearchFruitsTask/Program.cs
@@ -13,6 +13,20 @@
             
             // You must write your own linear search routine and not use any built-in fuctions available in C#
             // You should use meaningful variable names
+            using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		string[] fruits = { "banana", "apple", "orange", "pear", "grape", "pineapple" };
+		Console.WriteLine("Which fruit do you want to find?");
+		string fruit = Console.ReadLine();
+		if (fruit = fruits[0] | fruits[1] | fruits[2] | fruits[3] | fruits[4] | fruits[5]);
+		
+
+	}
+}
         }
     }
 }
